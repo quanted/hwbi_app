2221d83f87ba73f7f404b8710d8bd49c5c40258f
kurtw555 <kurt.wolfe@gmail.com>
2017-01-17 11:42:19 -0500

    Fixed urls

--- urls.py
+++ urls.py
@@ -6,7 +6,6 @@ from datetime import datetime
 from django.conf.urls import url
 import django.contrib.auth.views
 
-
 from hwbi_app import views
 from hwbi_app import algorithms
 from hwbi_app import description
@@ -18,9 +17,9 @@ from hwbi_app import references
 
 urlpatterns = [
     url(r'^$', description.description_page, {'model': "hwbi"}),
-    url(r'^/input$', input.input_page, {'model': "hwbi"}),
-    url(r'^/map$', map.map_page, {'model': "hwbi"}),
-    url(r'^/algorithms$', algorithms.algorithm_page, {'model': "hwbi"}),
-    url(r'^/references$', references.references_page, {'model': "hwbi"}),
+    url(r'^input/$', input.input_page, {'model': "hwbi"}),
+    url(r'^map/$', map.map_page, {'model': "hwbi"}),
+    url(r'^algorithms/$', algorithms.algorithm_page, {'model': "hwbi"}),
+    url(r'^references/$', references.references_page, {'model': "hwbi"}),
 
 ]
