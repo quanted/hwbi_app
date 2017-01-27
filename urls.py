0135a8bbf621c7755afbb45e7ff531b5ff8b5db4
AIgnatius <arignatius@gmail.com>
2017-01-27 13:32:56 -0500

    add OPEN API (swagger) /rest and /swag

--- urls.py
+++ urls.py
@@ -15,6 +15,7 @@ import links_left
 import map
 import references
 import singlepage
+import rest
 
 urlpatterns = [
     url(r'^$', description.description_page, {'model': "hwbi"}),
@@ -23,4 +24,14 @@ urlpatterns = [
     url(r'^map$', map.map_page, {'model': "hwbi"}),
     url(r'^algorithms$', algorithms.algorithm_page, {'model': "hwbi"}),
     url(r'^references$', references.references_page, {'model': "hwbi"}),
+    url(r'^rest$', rest.rest_page, {'model': "hwbi"}),
+    url(r'^test$', rest.rest_page_test, {'model': "hwbi"}),
+    url(r'^swag$', views.getSwaggerJsonContent)
 ]
+
+# 404 Error view (file not found)
+handler404 = views.file_not_found
+# 500 Error view (server error)
+handler500 = views.file_not_found
+# 403 Error view (forbidden)
+handler403 = views.file_not_found
--- views.py
+++ views.py
@@ -18,8 +18,8 @@ header = 'HWBI'
 # ####################################################################
 
 
-def hwbi_view(request):
-    html = render_to_string('index.html', {})
+def getSwaggerJsonContent(request):
+    html = render_to_string('hwbi_swagger.json', {})
     response = HttpResponse()
     response.write(html)
 
@@ -92,3 +92,24 @@ def get_user_HWBI_values(request):
 
     else:
         return HttpResponse({'error': 'Request must by GET'}, content_type="application/json")
+
+
+
+def file_not_found(request):
+        html = render_to_string('01uberheader_main_drupal.html', {
+            'SITE_SKIN': os.environ['SITE_SKIN'],
+            'TITLE': 'Error'})
+        html += render_to_string('02uberintroblock_wmodellinks_drupal.html', {
+            'CONTACT_URL': os.environ['CONTACT_URL']})
+        html += render_to_string('04ubertext_start_index_drupal.html', {
+            'TITLE': 'Error Processing Request',
+            'TEXT_PARAGRAPH': ""})
+        html += """<br><img src="/static/images/404error.png" width="300" height="300">"""
+        html += render_to_string('04ubertext_end_drupal.html', {})
+        html += links_left.ordered_list()
+        html += render_to_string('06uberfooter.html', {})
+
+        response = HttpResponse()
+        response.write(html)
+
+        return response
