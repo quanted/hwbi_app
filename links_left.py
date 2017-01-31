e15411fa6e37a7e5290a954ceda98103217c60a6
AIgnatius <arignatius@gmail.com>
2017-01-31 10:58:18 -0500

    edit links left reference to call unique hwbi html

--- links_left.py
+++ links_left.py
@@ -1,5 +1,6 @@
 from django.template.loader import render_to_string
 from collections import OrderedDict
+from django.shortcuts import redirect
 
 
 # 03ubertext_links_left:
@@ -10,14 +11,14 @@ def ordered_list(model=None, page=None):
             ])
         ),
         ('Documentation', OrderedDict([
-                ('API Documentation', 'hwbi/rest'),
-                ('Source Code', 'https://github.com/USEPA/HWBI')
+                ('API Documentation', '/rest')
+                # ('Source Code', 'https://github.com/USEPA/HWBI')
             ])
         )
     ])
 
     #return render_to_string('hwbi/03ubertext_links_left_drupal.html', {
-    return render_to_string('03ubertext_links_left_drupal.html', {
+    return render_to_string('03hwbi_links_left_drupal.html', {
         'LINK_DICT': link_dict,
         'MODEL': model,
         'PAGE': page
